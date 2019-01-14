16a9ed92f03d68e3d00f9228c6b855cfc369d20c
Razina Ali <razinali99@gmail.com>
2019-01-14 00:11:48 -0800

    hw completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,89 @@
+//Variables for wins & losses
+var wins = 0;
+var losses = 0;
+
+//The computer's random number for current round
+var computerNum = [];
+
+//Random number assigned to crystals for current round
+var blueNum;
+var pinkNum;
+var purpleNum;
+var redNum;
+
+//The player's score counter for the current round; add 0 & player 
+var playerCounter = 0;
+
+//**start the game**//
+function startGame() {
+	var compNum = Math.floor(Math.random() * 102) + 19;
+	computerNum.push(compNum);
+	$("#random-number").text(computerNum[0]);
+	
+	//Random number for crystals
+	blueNum = Math.floor(Math.random() * 12) + 1;
+
+	purpleNum = Math.floor(Math.random() * 12) + 1;
+
+	pinkNum = Math.floor(Math.random() * 12) + 1;
+
+	redNum = Math.floor(Math.random() * 12) + 1;
+};
+
+// reset the game
+function nextGame() {
+	computerNum.length = 0;
+	playerCounter = 0;
+	startGame();
+};
+
+//Function to determine player counter 
+function checkingNum () {
+	if (playerCounter === computerNum[0]) {
+		wins++;
+		$("#outcome").html("You won!!");
+		$("#wins").html("Wins: "+ wins);
+		nextGame();
+	}
+
+	else if (playerCounter > computerNum[0]) {
+		losses++;
+		$("#outcome").html("You lost!!");
+		$("#losses").html("Losses: " + losses);
+		nextGame();
+	}
+};
+
+// start
+startGame();
+
+//Clicking crystals
+$("#crystal-blue").on("click", function() {
+	playerCounter = blueNum + playerCounter;
+	$("#player-count").text(playerCounter);
+	console.log(playerCounter);
+	checkingNum();
+});
+
+$("#crystal-teal").on("click", function() {
+	playerCounter = purpleNum + playerCounter;
+	$("#player-count").text(playerCounter);
+	console.log(playerCounter);
+	checkingNum();
+});
+
+$("#crystal-clear").on("click", function() {
+	playerCounter = pinkNum + playerCounter;
+	$("#player-count").text(playerCounter);
+	console.log(playerCounter);
+	checkingNum();
+});
+
+$("#crystal-green").on("click", function() {
+	playerCounter = redNum + playerCounter;
+	$("#player-count").text(playerCounter);
+	console.log(playerCounter);
+	checkingNum();
+});
+
+});
